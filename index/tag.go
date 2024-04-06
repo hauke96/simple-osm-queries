d51051274f39c9d524a3f5854b4947f4e695dbfe
Hauke Stieler <mail@hauke-stieler.de>
2024-04-06 08:43:19 +0200

    Move import function into TagIndex struct

--- index/tag.go
+++ index/tag.go
@@ -3,13 +3,17 @@ package index
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"github.com/hauke96/sigolo/v2"
 	"github.com/paulmach/osm"
+	"github.com/paulmach/osm/osmpbf"
+	"github.com/paulmach/osm/osmxml"
 	"github.com/pkg/errors"
 	"io"
 	"os"
 	"sort"
 	"strings"
+	"time"
 )
 
 const tagIndexFilename = "./tag-index"
@@ -18,18 +22,6 @@ const NotFound = -1
 type TagIndex struct {
 	keyMap   []string   // The index value of a key is the position in this array.
 	valueMap [][]string // Array index is here the key index. I.e. valueMap[key] contains the list of value strings.
-}
-
-func NewTagIndex(keyMap []string, valueMap [][]string) *TagIndex {
-	for i, values := range valueMap {
-		sort.Strings(values)
-		valueMap[i] = values
-	}
-
-	return &TagIndex{
-		keyMap:   keyMap,
-		valueMap: valueMap,
-	}
 }
 
 func LoadTagIndex() (*TagIndex, error) {
@@ -67,10 +59,96 @@ func LoadTagIndex() (*TagIndex, error) {
 		return nil, errors.Wrapf(err, "Error while scanning tag-index file %s", tagIndexFilename)
 	}
 
-	index := NewTagIndex(keyMap, valueMap)
+	index := &TagIndex{
+		keyMap:   keyMap,
+		valueMap: valueMap,
+	}
 	index.Print()
 
 	return index, nil
+}
+
+func (i *TagIndex) Import(inputFile string) {
+	if !strings.HasSuffix(inputFile, ".osm") && !strings.HasSuffix(inputFile, ".pbf") {
+		sigolo.Error("Input file must be an .osm or .pbf file")
+		os.Exit(1)
+	}
+
+	f, err := os.Open(inputFile)
+	sigolo.FatalCheck(err)
+	defer f.Close()
+
+	var scanner osm.Scanner
+	if strings.HasSuffix(inputFile, ".osm") {
+		scanner = osmxml.New(context.Background(), f)
+	} else if strings.HasSuffix(inputFile, ".pbf") {
+		scanner = osmpbf.New(context.Background(), f, 1)
+	}
+	defer scanner.Close()
+
+	sigolo.Debug("Start processing tags from input data")
+	importStartTime := time.Now()
+
+	// TODO Use maps to quickly find keys and tags
+	var keyMap []string     // [key-index] -> key-string
+	var valueMap [][]string // [key-index][value-index] -> value-string
+
+	for scanner.Scan() {
+		obj := scanner.Object()
+		switch osmObj := obj.(type) {
+		case *osm.Node:
+			for _, tag := range osmObj.Tags {
+				// Search for the given key in the key map to get its index
+				keyIndex := -1
+				for i, k := range keyMap {
+					if k == tag.Key {
+						keyIndex = i
+						break
+					}
+				}
+
+				if keyIndex != -1 {
+					// Key already exists and so does its value map. Check is value already appeared and if not, add it.
+					containsValue := false
+					for _, value := range valueMap[keyIndex] {
+						if value == tag.Value {
+							containsValue = true
+							break
+						}
+					}
+					if !containsValue {
+						// Value not yet seen -> Add to value-map
+						valueMap[keyIndex] = append(valueMap[keyIndex], tag.Value)
+					}
+				} else {
+					// Key appeared for the first time -> Create maps and add entry
+					keyMap = append(keyMap, tag.Key)
+					valueMap = append(valueMap, []string{tag.Value})
+				}
+			}
+		}
+		// TODO Implement way handling
+		//case *osm.Way:
+		// TODO Implement relation handling
+		//case *osm.Relation:
+	}
+
+	// Make sure the values are sorted so that comparison operators work. We can change the order as we want, because
+	// OSM objects are not yet stored, this happens in a separate index.
+	for i, values := range valueMap {
+		sort.Strings(values)
+		valueMap[i] = values
+	}
+
+	i.keyMap = keyMap
+	i.valueMap = valueMap
+
+	importDuration := time.Since(importStartTime)
+	i.Print()
+	sigolo.Debugf("Created indices from OSM data in %s", importDuration)
+
+	err = i.SaveToFile()
+	sigolo.FatalCheck(err)
 }
 
 // GetKeyIndexFromKeyString returns the numerical index representation of the given key string and "NotFound" if the key doesn't exist.
