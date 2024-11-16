d35e02b39ca11dba1b9364cfe9c26a1cb558d3f5
Hauke Stieler <mail@hauke-stieler.de>
2024-11-16 17:26:25 +0100

    Remove not used source file

--- src/common/log.go
+++
@@ -1,7 +0,0 @@
-package common
-
-import "github.com/hauke96/sigolo/v2"
-
-func LogFatalBug(format string, args ...interface{}) {
-	sigolo.Fatalb(1, format+" - This is a bug, please report it at https://github.com/hauke96/sigolo/issues/new", args)
-}
